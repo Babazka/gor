f6e6513b1bc3dae8fea6aab525f4f24c7dd6f2af
Alexey Kryuchkov <alexey.kruchkov@gmail.com>
2013-10-17 13:33:22 +0400

    shell scripts

---
+++ clone.inc.sh
@@ -0,0 +1,44 @@
+# general functions for traffic cloning setup
+
+function kill_old_replays() {
+    if [ -f $FILE_WITH_PIDS ]
+    then
+        set +e
+        cat $FILE_WITH_PIDS | xargs kill
+        set -e
+    fi
+}
+
+function start_replays() {
+    echo "staring #<$REPLAY_COUNT> replay servers with #<$THREADS> threads and backlog of size #<$BACKLOG>"
+
+    rm -f $FILE_WITH_PIDS
+
+    for (( i=0; i <= $(($REPLAY_COUNT-1)); i++ ))
+    do
+        echo "start replay server $i"
+        python replay.py $STATSD \
+            --socket="$UNIX_SOCKET_PATH.$i" \
+            --threads $THREADS --backlog $BACKLOG \
+            --upstream "$DEST_HOST_PORT" > /dev/null &
+        REPLAY_PID=$!
+        echo $REPLAY_PID >>$FILE_WITH_PIDS
+        echo "pid = $REPLAY_PID started"
+    done
+
+    ps ax | grep "gor replay" | wc -l
+}
+
+function start_listener() {
+    echo "starting listener"
+    GOMAXPROCS=6 ./gor listen $STATSD \
+        -i $IFACE --pcap-filter "$PCAP_FILTER" \
+        -r "$UNIX_SOCKET_PATH|$L_HTTP_LIMIT" \
+        --pool-size $REPLAY_COUNT --packet-limit $L_PACKET_LIMIT \
+        --multiply $TRAFFIC_MULTIPLIER \
+        --die-if-replay-server-is-unreachable
+}
+
+kill_old_replays
+start_replays
+start_listener
---
+++ clone_example.sh
@@ -0,0 +1,51 @@
+#!/bin/bash -e
+#
+# ./clone_example.sh
+#
+#
+
+# [ cmdline parameters ] ============================================
+#
+# number of replay servers (12 seems good for 5..15K RPS)
+REPLAY_COUNT=12
+# number of greenlet threads per replay server
+THREADS=60
+
+# string to insert in unix socket names to avoid conflicts with
+# other traffic cloning projects
+PROJECT=logger
+# base path for unix sockets (".#socket_number" suffix will be appended)
+UNIX_SOCKET_PATH=/tmp/gor.$PROJECT
+# this file contains pids of all replay servers, one per line
+FILE_WITH_PIDS=$PROJECT.pids
+
+# [ traffic source and destination, limits ] ==========================
+
+# where is the traffic
+IFACE=eth1
+
+# what traffic should we clone
+PCAP_FILTER='(dst net 192.168.7.96/27) && ((tcp dst port 80) || (tcp dst portrange 9090-9100))'
+
+# multiply captured requests N times
+TRAFFIC_MULTIPLIER=1
+
+# who recieves cloned traffic
+DEST_HOST_PORT=192.168.142.132:80
+
+# limits on sniffing:
+# IP packets per second
+L_PACKET_LIMIT=100000
+# HTTP requests per second
+L_HTTP_LIMIT=25000
+
+
+# [ misc settings ] ==================================================
+#
+# size of backlog queue in the replay server, in requests
+# (when it's full, requests are dropped)
+BACKLOG=8000
+# statsd host
+STATSD=--statsd=192.168.142.31:8125
+
+source clone.inc.sh
